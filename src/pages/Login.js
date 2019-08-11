41e704a97cf6239b4df61a85a9b1abcdafaee705
Mhayk Whandson <hi@mhayk.com>
2019-08-11 10:32:26 -0400

    assets and login page has been done.
    Signed-off-by: Mhayk Whandson <hi@mhayk.com>

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,25 +1,71 @@
-import React from 'react';
-import { View, Text, StyleSheet } from "react-native";
+import React from "react";
+import {
+  KeyboardAvoidingView,
+  Platform,
+  Text,
+  StyleSheet,
+  Image,
+  TextInput,
+  TouchableOpacity
+} from 'react-native';
+
+import logo from "../assets/logo.png";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Hello World</Text>
-    </View>
+    <KeyboardAvoidingView
+      behavior="padding"
+      enabled={Platform.OS === "ios"}
+      style={styles.container}
+    >
+      <Image source={logo} />
+
+      <TextInput
+        autoCapitalize="none"
+        autoCorrect={false}
+        placeholder="Digite seu usuário no Github"
+        placeholderTextColor="#999"
+        style={styles.input}
+      />
+
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>Enviar</Text>
+      </TouchableOpacity>
+    </KeyboardAvoidingView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: 'row',
-    backgroundColor: '#00f',
-    justifyContent: 'center',
-    alignItems: "center",
+    backgroundColor: '#f5f5f5',
+    justifyContent: "center",
+    alignItems: 'center',
+    padding: 30
   },
-  text: {
-    fontWeight: "bold",
-    color: "#FFF",
-    fontSize: 20,
+  input: {
+    height: 46,
+    alignSelf: "stretch",
+    backgroundColor: "#FFF",
+    borderWidth: 1,
+    borderColor: "#ddd",
+    borderRadius: 4,
+    marginTop: 20,
+    paddingHorizontal: 15,
+  },
+  button: {
+    height: 46,
+    alignSelf: 'stretch',
+    backgroundColor: '#DF4723',
+    borderRadius: 4,
+    marginTop: 10,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+
+  buttonText: {
+    color: '#FFF',
+    fontWeight: 'bold',
+    fontSize: 16
   },
 });
