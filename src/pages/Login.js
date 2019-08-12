58491162f87c832de06fb0bc6fec84f651769253
Mhayk Whandson <hi@mhayk.com>
2019-08-12 12:56:31 +0100

    list users .. like and dislike has been  done.
    Signed-off-by: Mhayk Whandson <hi@mhayk.com>

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from "react";
-import AsyncStorage from '@react-native-community/async-storage';
+import React, { useState, useEffect } from 'react';
+import AsyncStorage from "@react-native-community/async-storage";
 import {
   KeyboardAvoidingView,
   Platform,
@@ -7,39 +7,39 @@ import {
   StyleSheet,
   Image,
   TextInput,
-  TouchableOpacity
-} from 'react-native';
+  TouchableOpacity,
+} from "react-native";
 
-import logo from "../assets/logo.png";
-import api from "../services/api";
+import logo from '../assets/logo.png';
+import api from '../services/api';
 
 export default function Login({ navigation }) {
-  const [user, setUser] = useState("");
+  const [user, setUser] = useState('');
 
   useEffect(() => {
-    AsyncStorage.getItem('user').then(user => {
+    AsyncStorage.getItem("user").then(user => {
       if (user) {
-        navigation.navigate('Main', { user });
+        navigation.navigate("Main", { user });
       }
     });
   }, [navigation]);
 
   async function handleLogin() {
-    const response = await api.post('/devs', { username: user });
+    const response = await api.post("/devs", { username: user });
 
     const { _id } = response.data;
 
-    await AsyncStorage.setItem('user', _id);
+    await AsyncStorage.setItem("user", _id);
 
     console.log(_id);
 
-    navigation.navigate('Main', { _id });
+    navigation.navigate("Main", { user: _id });
   }
 
   return (
     <KeyboardAvoidingView
       behavior="padding"
-      enabled={Platform.OS === "ios"}
+      enabled={Platform.OS === 'ios'}
       style={styles.container}
     >
       <Image source={logo} />
@@ -64,34 +64,34 @@ export default function Login({ navigation }) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#f5f5f5',
-    justifyContent: "center",
-    alignItems: 'center',
-    padding: 30
+    backgroundColor: "#f5f5f5",
+    justifyContent: 'center',
+    alignItems: "center",
+    padding: 30,
   },
   input: {
     height: 46,
-    alignSelf: "stretch",
-    backgroundColor: "#FFF",
+    alignSelf: 'stretch',
+    backgroundColor: '#FFF',
     borderWidth: 1,
-    borderColor: "#ddd",
+    borderColor: '#ddd',
     borderRadius: 4,
     marginTop: 20,
-    paddingHorizontal: 15,
+    paddingHorizontal: 15
   },
   button: {
     height: 46,
-    alignSelf: 'stretch',
-    backgroundColor: '#DF4723',
+    alignSelf: "stretch",
+    backgroundColor: "#DF4723",
     borderRadius: 4,
     marginTop: 10,
-    justifyContent: 'center',
-    alignItems: 'center'
+    justifyContent: "center",
+    alignItems: "center",
   },
 
   buttonText: {
-    color: '#FFF',
-    fontWeight: 'bold',
-    fontSize: 16
-  },
+    color: "#FFF",
+    fontWeight: "bold",
+    fontSize: 16,
+  }
 });
